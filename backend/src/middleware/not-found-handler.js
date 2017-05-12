66c082270f90b89623aaf0e6016993cb2d906940
Vitaliy <s.k.a.y.t1997@gmail.com>
2017-05-11 22:23:05 -0400

    production

--- backend/src/middleware/not-found-handler.js
+++ backend/src/middleware/not-found-handler.js
@@ -2,8 +2,11 @@
 
 const errors = require('feathers-errors');
 
+// not found errors should be handled by frontend
 module.exports = function() {
   return function(req, res, next) {
-    next(new errors.NotFound('Page not found'));
+    // next(new errors.NotFound('Page not found'));
+    res.sendFile(path.join(app.get('public'), 'index.html')));
+    next();
   };
 };
